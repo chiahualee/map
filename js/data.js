7462e5f4f285be3bf8b58aed3f61f64fd272b85e
chiahualee0321 <chiahualeeminions@gmail.com>
2016-06-16 14:18:30 +0800

    DONE!

--- js/data.js
+++ js/data.js
@@ -92,3 +92,52 @@ var route1_places = [
 {name:"道頓堀水上觀光船", location:{lat:34.669334, lng:135.502680},
     links:[{title:"[大阪行程] 大阪周遊卡~道頓崛水上觀光船＊ 體驗大阪夜色之美，五光十色(16)",link:"http://wkitty.pixnet.net/blog/post/32700143"}]
 }]
+var route2_places = [
+    {name:"通天閣", location:{lat:34.652729, lng:135.506295},
+    links:[{title:"【關西自由行●大阪】大阪周遊卡○瘋狂鐵腿一日遊○新世界、やまと屋串炸、通天閣 ",link:"http://tigercat0530.pixnet.net/blog/post/181919172"},
+    {title:"【大阪周遊卡】通天閣＊ 參觀大阪的歷史風情，登高眺望美景(15)",link:"http://wkitty.pixnet.net/blog/post/32691455"}]
+},
+    {name:"道頓堀水上觀光船", location:{lat:34.669334, lng:135.502680},
+    links:[{title:"[大阪行程] 大阪周遊卡~道頓崛水上觀光船＊ 體驗大阪夜色之美，五光十色(16)",link:"http://wkitty.pixnet.net/blog/post/32700143"}]
+},
+    {name:"大阪城天守閣", location:{lat:34.687545, lng:135.526244},
+    links:[{title:"【關西行程】大阪城天守閣＊ 大阪歷史傳承的象徵(9)",link:"http://wkitty.pixnet.net/blog/post/32614670"},{title:"【關西自由行●大阪】大阪周遊卡○瘋狂鐵腿一日遊○大阪城天守閣 ",link:"http://tigercat0530.pixnet.net/blog/post/182007444"}]
+},
+    {name:"HEP　FIVE摩天輪", location:{lat:34.704313, lng:135.500269},
+    links:[{title:"▌大阪自由行▌♥大阪周遊卡♥必玩11個熱門景點推薦與簡單行程規劃",link:"http://cline1413.pixnet.net/blog/post/406128199"},{title:"[大阪周遊券] Hep Five摩天輪＊紅色摩天輪浪漫滿分(7)",link:"http://wkitty.pixnet.net/blog/post/32571675"}]
+},
+    {name:"空中庭园展望台", location:{lat:34.705583, lng:135.490301}, 
+    links:[{title:"[大阪夜景] 梅田空中庭園景觀台(5)",link:"http://wkitty.pixnet.net/blog/post/32511030"},
+    {title:"▌大阪自由行▌♥大阪周遊卡♥必玩11個熱門景點推薦與簡單行程規劃",link:"http://cline1413.pixnet.net/blog/post/406128199"}]
+},
+    {name:"天然露天溫泉　SPA住之江", location:{lat:34.610710, lng:135.468800},
+    links:[]
+}
+]
+var route3_places = [
+    {name:"大阪生活今昔館", location:{lat:34.710631, lng:135.511506},
+    links:[{title:"【大阪生活今昔館】不用跑京都，在大阪體驗穿美美浴衣(11)",link:"http://wkitty.pixnet.net/blog/post/32636195"}
+    ,{title:"【關西自由行●大阪】大阪周遊卡○瘋狂鐵腿一日遊○大阪生活今昔館(大阪市立人居博物館) ",link:"http://tigercat0530.pixnet.net/blog/post/182083494"}
+    ,{title:"▌大阪自由行▌♥大阪周遊卡♥必玩11個熱門景點推薦與簡單行程規劃",link:"http://cline1413.pixnet.net/blog/post/406128199"}]
+},
+    {name:"帆船型觀光船 聖瑪麗亞號", location:{lat:34.655326, lng:135.428868},
+    links:[{title:"【大阪周遊卡】聖瑪麗亞號 ＊ 跟著哥倫布發現新大陸(12)",link:"http://wkitty.pixnet.net/blog/post/32654795"}]
+},
+    {name:"大阪歷史博物館", location:{lat:34.682879, lng:135.520913},
+    links:[{title:"[遊記]日本大阪歷史博物館-難波宮跡公園|大阪周遊卷免",link:"http://yopalit.pixnet.net/blog/post/41039038"},{title:"【關西自由行●大阪】大阪周遊卡○瘋狂鐵腿一日遊○大阪歷史博物館(大推)",link:"http://tigercat0530.pixnet.net/blog/post/182001540"}]
+},
+    {name:"大阪城天守閣", location:{lat:34.687545, lng:135.526244},
+    links:[{title:"【關西行程】大阪城天守閣＊ 大阪歷史傳承的象徵(9)",link:"http://wkitty.pixnet.net/blog/post/32614670"},{title:"【關西自由行●大阪】大阪周遊卡○瘋狂鐵腿一日遊○大阪城天守閣 ",link:"http://tigercat0530.pixnet.net/blog/post/182007444"}]
+},
+    {name:"四天王寺", location:{lat:34.653355, lng:135.516427},
+    links:[{title:"【關西自由行●大阪】大阪周遊卡○瘋狂鐵腿一日遊○四天王寺(中心伽藍)、釣鐘燒 ",link:"http://tigercat0530.pixnet.net/blog/post/181873164"}]
+},
+    {name:"通天閣", location:{lat:34.652729, lng:135.506295},
+    links:[{title:"【關西自由行●大阪】大阪周遊卡○瘋狂鐵腿一日遊○新世界、やまと屋串炸、通天閣 ",link:"http://tigercat0530.pixnet.net/blog/post/181919172"},
+    {title:"【大阪周遊卡】通天閣＊ 參觀大阪的歷史風情，登高眺望美景(15)",link:"http://wkitty.pixnet.net/blog/post/32691455"}]
+},
+    {name:"空中庭园展望台", location:{lat:34.705583, lng:135.490301}, 
+    links:[{title:"[大阪夜景] 梅田空中庭園景觀台(5)",link:"http://wkitty.pixnet.net/blog/post/32511030"},
+    {title:"▌大阪自由行▌♥大阪周遊卡♥必玩11個熱門景點推薦與簡單行程規劃",link:"http://cline1413.pixnet.net/blog/post/406128199"}]
+}
+]
--- js/maps.js
+++ js/maps.js
@@ -1,9 +1,13 @@
 var map;
 var map2;
+var map3;
+var map4;
 var infoWindow; // 顯示在marker上的window 
 // var markers = []; // 要顯示的所有marker
 
 var map2_markers = []
+var map3_markers = []
+var map4_markers = []
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
@@ -28,12 +32,41 @@ function initMap() {
     navigationControl: false,
     mapTypeControl: false,
     scaleControl: false,
-    disableDoubleClickZoom: true
+    disableDoubleClickZoom: true,
+        scaleControl: false,
+    scrollwheel: false
+  });
+  map3 = new google.maps.Map(document.getElementById('map3'), {
+    center: { // 將預設地圖中心放在以下地點
+      lat: 34.667936,
+      lng: 135.502151
+    },
+    zoom: 12,
+    streetViewControl: false,
+    navigationControl: false,
+    mapTypeControl: false,
+    scaleControl: false,
+    disableDoubleClickZoom: true,
+        scaleControl: false,
+    scrollwheel: false
+  });
+  map4 = new google.maps.Map(document.getElementById('map4'), {
+    center: { // 將預設地圖中心放在以下地點
+      lat: 34.667936,
+      lng: 135.502151
+    },
+    zoom: 12,
+    streetViewControl: false,
+    navigationControl: false,
+    mapTypeControl: false,
+    scaleControl: false,
+    disableDoubleClickZoom: true,
+        scaleControl: false,
+    scrollwheel: false
   });
   // 產生一個infoWindow
   infoWindow = new google.maps.InfoWindow();
   addMarkersToMaps()
-
 }
 
 
@@ -80,18 +113,59 @@ function createMarker2(latlng, name, links) {
     infoWindow.setContent(content);
   });
 }
+function createMarker3(latlng, name, links) {
+  var marker = new google.maps.Marker({
+    position: latlng,
+    map: map3,
+    title: name
+  });
+  map3_markers.push(marker);
+  marker.addListener('linkClicked', function() {
+    infoWindow.open(map3, marker);
+    infoWindow.title = name;
+    var content = "<h2  class='firstHeading'>" + name + "</h2>"
+    if (links.length > 0) {
+      content += "<p>相關熱門文章</p>"
+      for (var j = 0; j < links.length; j++) {
+        content += "<p><a href='" + links[j]["link"] + "'>" + links[j]["title"] + "</a></p>"
+      }
+    }
+    infoWindow.setContent(content);
+  });
+}
+function createMarker4(latlng, name, links) {
+  var marker = new google.maps.Marker({
+    position: latlng,
+    map: map4,
+    title: name
+  });
+  map4_markers.push(marker);
+  marker.addListener('linkClicked', function() {
+    infoWindow.open(map4, marker);
+    infoWindow.title = name;
+
+    var content = "<h2  class='firstHeading'>" + name + "</h2>"
+    if (links.length > 0) {
+      content += "<p>相關熱門文章</p>"
+      for (var j = 0; j < links.length; j++) {
+        content += "<p><a href='" + links[j]["link"] + "'>" + links[j]["title"] + "</a></p>"
+      }
+    }
+    infoWindow.setContent(content);
+  });
+}
 /*
  * you may not change the codes below
  */
 
 //清除地圖上的markers
-function clearLocations() {
-  infoWindow.close();
-  for (var i = 0; i < markers.length; i++) {
-    markers[i].setMap(null);
-  }
-  markers = [];
-}
+// function clearLocations() {
+//   infoWindow.close();
+//   for (var i = 0; i < markers.length; i++) {
+//     markers[i].setMap(null);
+//   }
+//   markers = [];
+// }
 
 function addMarkersToMaps() {
   for (var i = 0; i < places.length; i++) {
@@ -118,6 +192,36 @@ function addMarkersToMaps() {
     document.getElementById(aid).addEventListener("click", function(event) {
       var n = event.target.getAttribute("data-aid")
       new google.maps.event.trigger(map2_markers[n], 'linkClicked');
+    });
+  }
+  for (var i = 0; i < route2_places.length; i++) {
+    var name = route2_places[i]["name"];
+    var location = route2_places[i]["location"];
+    var latlng = new google.maps.LatLng(
+      parseFloat(location["lat"]),
+      parseFloat(location["lng"])
+    );
+    var links = route2_places[i]["links"]
+    createMarker3(latlng, name, links);
+    aid = "route2-" + i
+    document.getElementById(aid).addEventListener("click", function(event) {
+      var n = event.target.getAttribute("data-aid")
+      new google.maps.event.trigger(map3_markers[n], 'linkClicked');
+    });
+  }
+  for (var i = 0; i < route3_places.length; i++) {
+    var name = route3_places[i]["name"];
+    var location = route3_places[i]["location"];
+    var latlng = new google.maps.LatLng(
+      parseFloat(location["lat"]),
+      parseFloat(location["lng"])
+    );
+    var links = route3_places[i]["links"]
+    createMarker4(latlng, name, links);
+    aid = "route3-" + i
+    document.getElementById(aid).addEventListener("click", function(event) {
+      var n = event.target.getAttribute("data-aid")
+      new google.maps.event.trigger(map4_markers[n], 'linkClicked');
     });
   }
 }
